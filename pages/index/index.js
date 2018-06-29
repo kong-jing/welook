b3822fa2db2201ca5bfd390484a9ee5dcdf13682
kongjing <kongjing6@gmail.com>
2018-06-29 20:54:51 +0800

    添加天气

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,18 +18,22 @@ const weatherColorMap = {
 }
 
 Page({
-  data:{
-    nowTemp:14,
-    nowWeather:'多云',
-    nowWeatherBackground:''
+  data: {
+    nowTemp: 14,
+    nowWeather: '多云',
+    nowWeatherBackground: '',
+    hourlyWeather: []
   },
-  onLoad(){
-    console.log("Hello world!")
-
+  onPullDownRefresh: function () {//下拉刷新的回调
+    this.getNow(() => {
+      wx.stopPullDownRefresh();
+    });
+  },
+  getNow(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city: '广州市' 
+        city: '广州市'
       },
       header: {
         'content-type': 'application/json'//默认值
@@ -38,31 +42,68 @@ Page({
       success: res => {
         console.log(res.data);
         let result = res.data.result;
-        let temp = result.now.temp;
-        let weather = result.now.weather;
-        console.log(temp, weather);
-        this.setData({
-          nowTemp: temp + '°',
-          nowWeather: weatherMap[weather],
-          nowWeatherBackground:'/images/'+weather+'-bg.png'
-        })
-        //动态设置导航栏的颜色
-        wx.setNavigationBarColor({
-          frontColor: '#000000',
-          backgroundColor: weatherColorMap[weather],
-          animation: {
-            duration: 400,
-            timingFunc: 'easeIn'
-          },
-          success: function(res) {},
-          fail: function(res) {},
-          complete: function(res) {},
-        })
-      },
-      fail: res =>{
+        this.setNow(result);
+        this.setHourlyWeather(result);
+        //设置forecast参数
 
+      },
+      fail: res => {
+        wx.stopPullDownRefresh();
+      },
+      complete: () => {
+        // wx.stopPullDownRefresh();
+        callback && callback();
       }
-  })
+    })
+  },
+  onLoad() {
+    console.log("Hello world!");
+    this.getNow();
+
+  },
+
+  // 设置当前时间 
+  setNow(result) {
+
+    let temp = result.now.temp;
+    let weather = result.now.weather;
+    console.log(temp, weather);
+    this.setData({
+      nowTemp: temp + '°',
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png'
+    })
+    //动态设置导航栏的颜色
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather],
+      animation: {
+        duration: 400,
+        timingFunc: 'easeIn'
+      },
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+
+  setHourlyWeather(result) {
+    let forecast = result.forecast;
+    //设置时间
+    let nowHour = new Date().getHours()
+
+    let hourlyWeather = [];
+    for (let i = 0; i < 8; i += 1) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/images/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°'
+      });
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      hourlyWeather: hourlyWeather
+    })
   }
 })
 
